f450016b74c185a836dc3f308cf77274eadf251e
Mohanraj Gopi <mohanraj.gopi@perficient.com>
2023-07-28 17:54:46 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+dotenv.config();
+
+mongoose.connect("mongodb://localhost:27017/user", { useNewUrlParser: true });
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+app.listen(3500, () => {
+  console.log("Backend server is running");
+});
